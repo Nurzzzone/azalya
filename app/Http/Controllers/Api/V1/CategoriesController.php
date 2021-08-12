7ec158cedfdef3fa7893fdbfd4c03b0c541345f1
nurzzzone <nurzzzone@gmail.com>
2021-08-12 12:08:20 +0600

    feat: add categories show api

--- app/Http/Controllers/Api/V1/CategoriesController.php
+++ app/Http/Controllers/Api/V1/CategoriesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Category;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductCollection;
 use App\Http\Resources\CategoriesCollection;
 
 class CategoriesController extends Controller
@@ -17,6 +18,20 @@ class CategoriesController extends Controller
     {
         return (new CategoriesCollection((self::MODEL)::all()))
             ->response()
+            ->setEncodingOptions(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    }
+
+    /**
+     * @param string $category_slug
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($category_slug)
+    {
+        $products = Category::where('slug', $category_slug)
+            ->first()
+            ->products
+            ->where('is_active', true);
+        return (new ProductCollection($products))->response()
             ->setEncodingOptions(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'v1', 'namespace' => 'Api\\V1'], function() {
     Route::get('/products', 'ProductsController@index');
     Route::get('/categories', 'CategoriesController@index');
+    Route::get('/categories/{category_slug}', 'CategoriesController@show');
     Route::get('/members', 'MemberController@index');
 });
