5f84393548399a7ee35fe17a412416faeb892a36
nurzzzone <nurzzzone@gmail.com>
2021-08-12 13:54:38 +0600

    refactor: change crud generator

--- app/Console/Commands/CreateViewCommand.php
+++ app/Console/Commands/CreateViewCommand.php
@@ -31,8 +31,8 @@ class CreateViewCommand extends GeneratorCommand
         $model = $this->argument('besource');
         $resources = $this->getResource();
         $stubs = $this->getStub();
-        $this->createRequests($model);
-        $this->createController($model);
+        $requests = $this->createRequests($model);
+        $this->createController($model, $requests);
         $path = $this->createDir($model);
 
         # ========================================== #
@@ -92,21 +92,26 @@ class CreateViewCommand extends GeneratorCommand
         return $path;
     }
 
-    protected function createController($model)
+    protected function createController($model, $requests)
     {
+        $route = $model;
+        $object = $model;
         if (str_contains($model, '\\')) {
             $mapped = array_map(fn($item) => ucfirst($item), explode('\\', $model));
+            $route = str_replace('\\', '.', $model);
+            $for_object = explode('.', $route);
+            $object = end($for_object);
             $model = implode('', $mapped);
         }
 
-        $filename = ucfirst($model)."Controller.php";
-        $path = "app\\Http\\Controllers\\Backend\\$filename";
+        $filename = ucfirst($model);
+        $path = "app\\Http\\Controllers\\Backend";
 
-        if (File::exists($path)) {
-            $this->line("$filename is already exists!");
-        }
-        $content = File::get("app\\Console\\Stubs\\custom-controller.stub");
-        File::put($path, $content);
+        $components = $this->getControllerComponents($filename, $path, $route, $object, $filename, $requests);
+        $replacements = ['{{class}}', '{{namespace}}', '{{route}}', '{{object}}', '{{model}}', '{{createRequest}}', '{{updateRequest}}'];
+        $replace_to = array_filter($components, fn($key) => $key !== 'filename' && $key !== 'content', ARRAY_FILTER_USE_KEY);
+        $this->replaceReference($components['content'], $replacements, $replace_to, "$path\\{$components['filename']}");
+        $this->info("{$components['class']} created!");
     }
 
     protected function createRequests($model)
@@ -118,31 +123,20 @@ class CreateViewCommand extends GeneratorCommand
             $dir_name = implode('\\', $ucfirsted);
             $model = end($exploded);
         }
-        $dir_name = ucfirst($model) . '\\';
+        $dir_name = ucfirst($model);
         $name = ucfirst($model);
 
-        $requests = [
-            [
-                'filename' => "Create{$name}Request.php",
-                'content' =>  "app\\Console\\Stubs\\create-request.stub"
-            ],
-            [
-                'filename' => "Update{$name}Request.php",
-                'content' => "app\\Console\\Stubs\\update-request.stub"
-            ]
-        ];
+        $requests = $this->getRequestsComponents($name, $dir_name);
+        $replacements = ['{{class}}', '{{namespace}}'];
+        $dir_path = $this->makeDirOptional("app\\Http\\Requests\\{$dir_name}");
 
-        $dir_path = "app\\Http\\Requests\\{$dir_name}";
-        if (!File::exists($dir_path)) {
-            File::makeDirectory($dir_path);
-        }
         foreach($requests as $request) {
-            if (!File::exists($dir_path . $request['filename'])) {
-                $content = File::get($request['content']);
-                File::put($dir_path . $request['filename'], $content);
+            if (!File::exists($request['namespace'] . $request['filename'])) {
+                $this->replaceReference($request['content'], $replacements, [$request['class'], $request['namespace']], "$dir_path \\ {$request['filename']}");
             }
             $this->line("{$request['filename']} is already exists!");
         }
+        return $requests;
     }
 
     protected function getStub(): array
@@ -156,5 +150,53 @@ class CreateViewCommand extends GeneratorCommand
             $carry[explode('.', $stub)[0]] = $dir . $stub;
             return $carry;
         }, []);
+    }
+
+    private function replaceReference($content_path, $find, $replace_to, $put_path)
+    {
+        $file_contents = file_get_contents($content_path);
+        $file_contents = str_replace($find, $replace_to, $file_contents);
+        file_put_contents($put_path, $file_contents);
+    }
+
+    private function getRequestsComponents($name, $dir_name)
+    {
+        return [
+            [
+                'filename' => "Create{$name}Request.php",
+                'class' => "Create{$name}Request",
+                'namespace' => "App\Http\Requests\\$dir_name",
+                'content' =>  "app\\Console\\Stubs\\create-request.stub"
+            ],
+            [
+                'filename' => "Update{$name}Request.php",
+                'class' => "Update{$name}Request",
+                'namespace' => "App\Http\Requests\\$dir_name",
+                'content' => "app\\Console\\Stubs\\update-request.stub",
+            ]
+        ];
+    }
+
+    private function getControllerComponents($name, $dir_path, $route, $object, $model, $requests)
+    {
+        return [
+            'filename' => "{$name}Controller.php",
+            'class' => "{$name}Controller",
+            'namespace' => "$dir_path",
+            'content' =>  "app\\Console\\Stubs\\custom-controller.stub",
+            'route' => $route,
+            'object' => $object,
+            'model' => $model,
+            'createRequest' => $requests[0]['class'],
+            'updateRequest' => $requests[1]['class'],
+        ];
+    }
+
+    private function makeDirOptional(string $dir_path)
+    {
+        if (!File::exists($dir_path)) {
+            File::makeDirectory($dir_path);
+        }
+        return $dir_path;
     }
 }
