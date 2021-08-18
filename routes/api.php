b899e26584c108a2a9990bd8415ea608d0b5095e
nurzzzone <nurzzzone@gmail.com>
2021-08-18 18:12:16 +0600

    refactor: изменен routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -12,21 +12,21 @@ Route::group(['prefix' => 'v1', 'namespace' => 'Api\\V1'], function() {
         Route::post('/access-token', 'AuthController@getToken');
     });
 
-    Route::group(['middleware' => ['jwt', 'auth:api']], function() {
-        Route::group(['prefix' => 'page'], function() {
-            Route::get('/home', 'HomepageController@page');
-            Route::get('/about', 'AboutController@page');
-            Route::get('/products', 'ProductsController@page');
-            Route::get('/product/{product}', 'ProductsController@show');
-            Route::get('/delivery', 'DeliveryController@page');
-            Route::get('/promotions', 'PromotionController@page');
-            Route::get('/contacts', 'ContactsController@page');
-        });
+    Route::group(['prefix' => 'page'], function() {
+        Route::get('/home', 'HomepageController@page');
+        Route::get('/about', 'AboutController@page');
+        Route::get('/products', 'ProductsController@page');
+        Route::get('/product/{product}', 'ProductsController@show');
+        Route::get('/delivery', 'DeliveryController@page');
+        Route::get('/promotions', 'PromotionController@page');
+        Route::get('/contacts', 'ContactsController@page');
+    });
+    
+    Route::get('/categories/{category_slug}', 'CategoriesController@show');
+    Route::post('/products/filter', 'ProductsController@filter');
 
-        Route::get('/categories/{category_slug}', 'CategoriesController@show');
-        Route::get('/delivery', 'DeliveryController@index');
+    Route::group(['middleware' => ['jwt', 'auth:api']], function() {
         Route::post('/products/{product}/favorites', 'ProductsController@toggleFavorite');
-        Route::post('/products/filter', 'ProductsController@filter');
         Route::get('/user/{user}/favorites', 'UserController@list');
         Route::post('/checkout', 'OrderController@checkout');
     });
